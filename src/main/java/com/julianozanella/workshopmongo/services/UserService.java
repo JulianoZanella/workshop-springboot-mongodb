774aca6577c953cfeb7ab1bacabd5ccdba1bf0d8
Juliano Zanella <juliano.zanella.live@gmail.com>
2019-02-23 06:14:13 -0300

    Atualizacao de usuario com PUT

--- src/main/java/com/julianozanella/workshopmongo/services/UserService.java
+++ src/main/java/com/julianozanella/workshopmongo/services/UserService.java
@@ -16,26 +16,37 @@ public class UserService {
 
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
-	
+
 	public User insert(User obj) {
 		obj.setId(null);
 		return repo.insert(obj);
 	}
-	
+
 	public void delete(String id) {
 		findById(id);
 		repo.deleteById(id);
 	}
-	
+
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	private void updateData(User newObj, User oldObj) {
+		newObj.setName(oldObj.getName() == null ? newObj.getName() : oldObj.getName());
+		newObj.setEmail(oldObj.getEmail() == null ? newObj.getEmail() : oldObj.getEmail());
+	}
+
 	public User fromDto(UserDTO obj) {
 		return new User(obj.getId(), obj.getName(), obj.getEmail());
 	}
